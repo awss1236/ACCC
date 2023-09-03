731b5ebf53063104a313ed467522e085688e0180
awss1236 <awsssayah@gmail.com>
2023-09-03 05:19:15 +0100

    Finished assembling for local variables.

--- app/CAssembler.hs
+++ app/CAssembler.hs
@@ -1,31 +1,44 @@
 module CAssembler where
 import CLexer
 import CParser
+import qualified Data.Map as M
 
-genExpAsm :: Exp -> String
-genExpAsm (Constant (i, _)) = "  movl   $" ++ show i ++ ", %eax\n"
-genExpAsm (UnaryAct ((UComp    , exp), _)) = genExpAsm exp ++ "  not    %eax\n"
-genExpAsm (UnaryAct ((UMinus   , exp), _)) = genExpAsm exp ++ "  neg    %eax\n"
-genExpAsm (UnaryAct ((ULogicNeg, exp), _)) = genExpAsm exp ++ "  cmpl   $0, %eax\n  movl   $0, %eax\n  sete   %al\n"
-genExpAsm (BinAct((BMult, e1, e2), _)) = genExpAsm e1 ++ "  push   %eax\n" ++ genExpAsm e2 ++ "  pop    %ecx\n  imul   %ecx, %eax\n"
-genExpAsm (BinAct((BDiv,  e1, e2), _)) = genExpAsm e1 ++ "  push   %eax\n" ++ genExpAsm e2 ++ "  movl   %eax, %ecx\n  pop    %eax\n  cdq\n  idivl   %ecx\n"
-genExpAsm (BinAct((BAdd,  e1, e2), _)) = genExpAsm e1 ++ "  push   %eax\n" ++ genExpAsm e2 ++ "  pop    %ecx\n  addl   %ecx, %eax\n"
-genExpAsm (BinAct((BSub,  e1, e2), _)) = genExpAsm e2 ++ "  push   %eax\n" ++ genExpAsm e1 ++ "  pop    %ecx\n  subl   %ecx, %eax\n"
-genExpAsm (BinAct((BAnd,  e1, e2), (x, y))) = let p = "_" ++ show x ++ "_" ++ show y ++ ":\n" in genExpAsm e1 ++ "  cmpl   $0, %eax\n  jne    _clause2" ++ p ++ "  jmp    _end" ++ p ++ "_clause2" ++ p ++ genExpAsm e2 ++ "  cmpl   $0, %eax\n  movl   $0, %eax\n  setne  %al\n_end" ++ p
-genExpAsm (BinAct((BOr,   e1, e2), (x, y))) = let p = "_" ++ show x ++ "_" ++ show y ++ ":\n" in genExpAsm e1 ++ "  cmpl   $0, %eax\n  je     _clause2" ++ p ++ "  movl   $1, %eax\n  jmp    _end" ++ p ++ "_clause2" ++ p ++ genExpAsm e2 ++ "  cmpl   $0, %eax\n  movl   $0, %eax\n  setne  %al\n_end" ++ p
-genExpAsm (BinAct((BEqu,  e1, e2), _)) = genExpAsm e1 ++ "  push   %eax\n" ++ genExpAsm e2 ++ "  pop    %ecx\n  cmpl   %eax, %ecx\n  movl   $0, %eax\n  sete   %al\n"
-genExpAsm (BinAct((BNqu,  e1, e2), _)) = genExpAsm e1 ++ "  push   %eax\n" ++ genExpAsm e2 ++ "  pop    %ecx\n  cmpl   %eax, %ecx\n  movl   $0, %eax\n  setne  %al\n"
-genExpAsm (BinAct((BLt,   e1, e2), _)) = genExpAsm e1 ++ "  push   %eax\n" ++ genExpAsm e2 ++ "  pop    %ecx\n  cmpl   %eax, %ecx\n  movl   $0, %eax\n  setl   %al\n"
-genExpAsm (BinAct((BGt,   e1, e2), _)) = genExpAsm e1 ++ "  push   %eax\n" ++ genExpAsm e2 ++ "  pop    %ecx\n  cmpl   %eax, %ecx\n  movl   $0, %eax\n  setg   %al\n"
-genExpAsm (BinAct((BLe,   e1, e2), _)) = genExpAsm e1 ++ "  push   %eax\n" ++ genExpAsm e2 ++ "  pop    %ecx\n  cmpl   %eax, %ecx\n  movl   $0, %eax\n  setle  %al\n"
-genExpAsm (BinAct((BGe,   e1, e2), _)) = genExpAsm e1 ++ "  push   %eax\n" ++ genExpAsm e2 ++ "  pop    %ecx\n  cmpl   %eax, %ecx\n  movl   $0, %eax\n  setge  %al\n"
+type StackState = (Int, M.Map String Int)
 
-genStatAsm :: Statement -> String
-genStatAsm (Return (exp, _)) = genExpAsm exp ++ "  ret\n"
+genExpAsm :: StackState -> Exp -> String
+genExpAsm _ (Constant (i, _)) = "  movl   $" ++ show i ++ ", %eax\n"
+genExpAsm m (UnaryAct ((UComp    , exp), _)) = genExpAsm m exp ++ "  not    %eax\n"
+genExpAsm m (UnaryAct ((UMinus   , exp), _)) = genExpAsm m exp ++ "  neg    %eax\n"
+genExpAsm m (UnaryAct ((ULogicNeg, exp), _)) = genExpAsm m exp ++ "  cmpl   $0, %eax\n  movl   $0, %eax\n  sete   %al\n"
+genExpAsm m (BinAct((BMult, e1, e2), _)) = genExpAsm m e1 ++ "  push   %eax\n" ++ genExpAsm m e2 ++ "  pop    %ecx\n  imul   %ecx, %eax\n"
+genExpAsm m (BinAct((BDiv,  e1, e2), _)) = genExpAsm m e1 ++ "  push   %eax\n" ++ genExpAsm m e2 ++ "  movl   %eax, %ecx\n  pop    %eax\n  cdq\n  idivl   %ecx\n"
+genExpAsm m (BinAct((BAdd,  e1, e2), _)) = genExpAsm m e1 ++ "  push   %eax\n" ++ genExpAsm m e2 ++ "  pop    %ecx\n  addl   %ecx, %eax\n"
+genExpAsm m (BinAct((BSub,  e1, e2), _)) = genExpAsm m e2 ++ "  push   %eax\n" ++ genExpAsm m e1 ++ "  pop    %ecx\n  subl   %ecx, %eax\n"
+genExpAsm m (BinAct((BAnd,  e1, e2), (x, y))) = let p = "_" ++ show x ++ "_" ++ show y ++ ":\n" in genExpAsm m e1 ++ "  cmpl   $0, %eax\n  jne    _clause2" ++ p ++ "  jmp    _end" ++ p ++ "_clause2" ++ p ++ genExpAsm m e2 ++ "  cmpl   $0, %eax\n  movl   $0, %eax\n  setne  %al\n_end" ++ p
+genExpAsm m (BinAct((BOr,   e1, e2), (x, y))) = let p = "_" ++ show x ++ "_" ++ show y ++ ":\n" in genExpAsm m e1 ++ "  cmpl   $0, %eax\n  je     _clause2" ++ p ++ "  movl   $1, %eax\n  jmp    _end" ++ p ++ "_clause2" ++ p ++ genExpAsm m e2 ++ "  cmpl   $0, %eax\n  movl   $0, %eax\n  setne  %al\n_end" ++ p
+genExpAsm m (BinAct((BEqu,  e1, e2), _)) = genExpAsm m e1 ++ "  push   %eax\n" ++ genExpAsm m e2 ++ "  pop    %ecx\n  cmpl   %eax, %ecx\n  movl   $0, %eax\n  sete   %al\n"
+genExpAsm m (BinAct((BNqu,  e1, e2), _)) = genExpAsm m e1 ++ "  push   %eax\n" ++ genExpAsm m e2 ++ "  pop    %ecx\n  cmpl   %eax, %ecx\n  movl   $0, %eax\n  setne  %al\n"
+genExpAsm m (BinAct((BLt,   e1, e2), _)) = genExpAsm m e1 ++ "  push   %eax\n" ++ genExpAsm m e2 ++ "  pop    %ecx\n  cmpl   %eax, %ecx\n  movl   $0, %eax\n  setl   %al\n"
+genExpAsm m (BinAct((BGt,   e1, e2), _)) = genExpAsm m e1 ++ "  push   %eax\n" ++ genExpAsm m e2 ++ "  pop    %ecx\n  cmpl   %eax, %ecx\n  movl   $0, %eax\n  setg   %al\n"
+genExpAsm m (BinAct((BLe,   e1, e2), _)) = genExpAsm m e1 ++ "  push   %eax\n" ++ genExpAsm m e2 ++ "  pop    %ecx\n  cmpl   %eax, %ecx\n  movl   $0, %eax\n  setle  %al\n"
+genExpAsm m (BinAct((BGe,   e1, e2), _)) = genExpAsm m e1 ++ "  push   %eax\n" ++ genExpAsm m e2 ++ "  pop    %ecx\n  cmpl   %eax, %ecx\n  movl   $0, %eax\n  setge  %al\n"
+genExpAsm m (Set((n, e), _)) = genExpAsm m e ++ "  movl   %eax, " ++ show (snd m M.! n) ++ "(%ebp)\n"
+genExpAsm m (Var(n, _)) = "  movl   " ++ show (snd m M.! n) ++ ", %eax\n"
+
+
+genStatAsm :: StackState -> Statement -> (String, StackState)
+genStatAsm im (Return (exp, _)) = (genExpAsm im exp ++ "  ret\n", im)
+genStatAsm im (Expr exp)        = (genExpAsm im exp, im)
+genStatAsm (i, m) (Declare ((n, Nothing), _)) = (if M.member n m then "Variable redeclaration is a fucked up thing bud" else "  pushl  $69\n", (i-4, M.insert n i m))
+genStatAsm (i, m) (Declare ((n, Just e ), _)) = (if M.member n m then "Variable redeclaration is a fucked up thing bud" else genExpAsm (i, m) e ++ "  pushl  %eax\n", (i-4, M.insert n i m))
 
 genFuncAsm :: Bool -> FunctionDecl -> String
-genFuncAsm False (FunctionDecl ((n, s), _)) = " .globl _"++n++"\n_"++n++":\n"++genStatAsm s
-genFuncAsm True  (FunctionDecl ((n, s), _)) = " .globl " ++n++"\n" ++n++":\n"++genStatAsm s
+genFuncAsm False (FunctionDecl ((n, s), _)) = " .globl _"++n++"\n_"++n++":\n"++ (fst (foldr (\stt (a, stk)-> let (na, nstk) = genStatAsm stk stt in (a ++ na, nstk)) ("", (-4, M.empty)) s)) ++ mReturnZero (last s)
+      where mReturnZero (Return _) = ""
+            mReturnZero _ = "  movl   $0, %eax\n  ret\n"
+genFuncAsm True  (FunctionDecl ((n, s), _)) = " .globl " ++n++"\n" ++n++":\n"++ (fst (foldr (\stt (a, stk)-> let (na, nstk) = genStatAsm stk stt in (a ++ na, nstk)) ("", (-4, M.empty)) s)) ++ mReturnZero (last s)
+      where mReturnZero (Return _) = ""
+            mReturnZero _ = "  movl   $0, %eax\n  ret\n"
 
 genProgAsm :: Bool -> Program -> String
 genProgAsm b (Program (f, _)) = genFuncAsm b f
