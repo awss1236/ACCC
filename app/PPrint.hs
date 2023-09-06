e96410eba9b7468faf54c9e32f8c0bc68a05b07d
awss1236 <awsssayah@gmail.com>
2023-09-06 22:31:47 +0100

    Upgraded pretty printing.

--- app/PPrint.hs
+++ app/PPrint.hs
@@ -27,15 +27,18 @@ pPrintExp (Set ((v, e), _)) = "($"++v ++ " = " ++ pPrintExp e ++ ")"
 pPrintExp (Constant (i, _)) = show i
 pPrintExp (UnaryAct ((o, e), _)) = pPrintUnary o ++ "(" ++ pPrintExp e ++ ")"
 pPrintExp (BinAct ((o, e1, e2), _)) = "(" ++ pPrintExp e1 ++ pPrintBinary o ++ pPrintExp e2 ++ ")"
+pPrintExp (Tern ((c, e1, e2), _)) = "(IF " ++ pPrintExp c ++ " THEN " ++ pPrintExp e1 ++ " ELSE " ++ pPrintExp e2 ++ ")"
 
-pPrintStat :: Statement -> [String]
-pPrintStat (Expr e) = [pPrintExp e]
-pPrintStat (Declare ((v, Nothing), _)) = ["DECLARE $" ++ v]
-pPrintStat (Declare ((v, Just e),  _)) = ["DECLARE $" ++ v ++ " = " ++ pPrintExp e]
-pPrintStat (Return (exp, _)) = ["RETURN "++pPrintExp exp]
+pPrintBlockItem :: BlockItem -> [String]
+pPrintBlockItem (Stat (Expr e)) = [pPrintExp e]
+pPrintBlockItem (Stat (Return (exp, _))) = ["RETURN "++pPrintExp exp]
+pPrintBlockItem (Stat (If ((e, s, Nothing), _))) = ("IF " ++ pPrintExp e ++ ":") : map ("  " ++) (pPrintBlockItem (Stat s))
+pPrintBlockItem (Stat (If ((e, s, Just s'), _))) = ("IF " ++ pPrintExp e ++ ":") : map ("  " ++) (pPrintBlockItem (Stat s)) ++ ("ELSE:" : map ("  " ++) (pPrintBlockItem (Stat s')))
+pPrintBlockItem (Decl (Declare ((v, Nothing), _))) = ["DECLARE $" ++ v]
+pPrintBlockItem (Decl (Declare ((v, Just e),  _))) = ["DECLARE $" ++ v ++ " = " ++ pPrintExp e]
 
 pPrintFunc :: FunctionDecl -> [String]
-pPrintFunc (FunctionDecl ((n, s), _)) = ["FUN INT "++n++":", "  params: ()", "  body:"] ++ concatMap (map ("    "++).pPrintStat) s
+pPrintFunc (FunctionDecl ((n, s), _)) = ["FUN INT "++n++":", "  params: ()", "  body:"] ++ concatMap (map ("  "++).pPrintBlockItem) s
 
 pPrintProg :: Program -> String
 pPrintProg (Program (f, _)) = foldl (\acc s -> acc ++ s ++ "\n") "" $ pPrintFunc f
