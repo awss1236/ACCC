378dc4eed8ddccf95b76f009317a2f31af63d8f9
awss1236 <awsssayah@gmail.com>
2023-09-10 02:45:00 +0100

    Fixed simple bugs.

--- app/CChecker.hs
+++ app/CChecker.hs
@@ -9,10 +9,10 @@ check (Program fs) = if checkMultDefs fs then Just "You tryna confuse me with al
                      Nothing
 
 checkMultDefs :: [FunctionDecl] -> Bool
-checkMultDefs fs = let fs' = map (\(FunctionDecl ((n, _, _), _)) -> n) $ filter (\(FunctionDecl ((_, _, b), _)) -> isJust b) fs in anySame fs'
+checkMultDefs fs = let fs' = map (\(FunctionDecl ((n, _, _), _)) -> n) $ filter (\(FunctionDecl ((_, _, b), _)) -> isJust b) fs in anySame (M.empty) fs'
                 where anySame :: M.Map String () -> [String] -> Bool
                       anySame _ [] = False
-                      anySame m (s:ss) = if M.member s m then True else anySame (m.insert s ()) ss
+                      anySame m (s:ss) = if M.member s m then True else anySame (M.insert s () m) ss
 
 checkIncoDefs :: [FunctionDecl] -> Bool
 checkIncoDefs fs = or (map f $ groupBy (\(FunctionDecl ((n1, _, _), _)) (FunctionDecl ((n2, _, _), _)) -> n1 == n2) fs)
@@ -20,3 +20,8 @@ checkIncoDefs fs = or (map f $ groupBy (\(FunctionDecl ((n1, _, _), _)) (Functio
                       allSame :: [Int] -> Bool
                       allSame [] = False
                       allSame (a:as) = if a `elem` as then allSame as else False
+                      groupBy :: (a -> a -> Bool) -> [a] -> [[a]]
+                      groupBy f as = foldr (ins f) [] as
+                      ins :: (a -> a -> Bool) -> a -> [[a]] -> [[a]]
+                      ins _ a [] = [[a]]
+                      ins f a ((b:r):r') = if f a b then ((a:b:r):r') else ((b:r):(ins f a r'))
