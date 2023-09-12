a593da21fd2f0f5ca0e527431009b1caa102d948
awss1236 <awsssayah@gmail.com>
2023-09-12 08:14:13 +0100

    Updated checker for global variables.

--- app/CChecker.hs
+++ app/CChecker.hs
@@ -4,19 +4,24 @@ import Data.Maybe
 import qualified Data.Map as M
 
 check :: Program -> Maybe String
-check (Program fs) = if checkMultDefs fs then Just "You tryna confuse me with all those redefinitions mate." else
-                     if checkIncoDefs fs then Just "Make up your damn mind about those funtion definitions." else
+check (Program ts) = if checkMultDefs toDef ts then Just "You tryna confuse me with all those redefinitions mate." else
+                     if checkIncoDefs toLs ts then Just "Make up your damn mind about those funtion definitions." else
                      Nothing
+                     where
+                       toDef (Func (FunctionDecl ((n, _, b), _))) = (n, isJust b)
+                       toDef (GVar (Declare ((n, _), _))) = (n, True)
+                       toLs  (Func (FunctionDecl ((n, a, _), _))) = (n, length a)
+                       toLs  (GVar (Declare ((n, _), _))) = (n, 1)
 
-checkMultDefs :: [FunctionDecl] -> Bool
-checkMultDefs fs = let fs' = map (\(FunctionDecl ((n, _, _), _)) -> n) $ filter (\(FunctionDecl ((_, _, b), _)) -> isJust b) fs in anySame (M.empty) fs'
+checkMultDefs :: (a -> (String, Bool)) -> [a] -> Bool
+checkMultDefs f fs = let fs' = map (fst.f) $ filter (snd.f) fs in anySame (M.empty) fs'
                 where anySame :: M.Map String () -> [String] -> Bool
                       anySame _ [] = False
                       anySame m (s:ss) = if M.member s m then True else anySame (M.insert s () m) ss
 
-checkIncoDefs :: [FunctionDecl] -> Bool
-checkIncoDefs fs = or (map f $ groupBy (\(FunctionDecl ((n1, _, _), _)) (FunctionDecl ((n2, _, _), _)) -> n1 == n2) fs)
-                where f ds = allSame $ map (\(FunctionDecl ((_, a, _), _)) -> length a) ds
+checkIncoDefs :: (a -> (String, Int)) -> [a] -> Bool
+checkIncoDefs f fs = or (map c $ groupBy (\a b -> (fst.f) a == (fst.f) b) fs)
+                where c ds = allSame $ map (snd.f) ds
                       allSame :: [Int] -> Bool
                       allSame [] = False
                       allSame (a:as) = if a `elem` as then allSame as else False
